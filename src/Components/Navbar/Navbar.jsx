0df8a822be9495987b82d4deddf3ed5be3f26ca9
Gourav Chatterjee <gouravchatterjee65@gmail.com>
2024-12-29 19:51:12 +0530

    design changes

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,19 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import ThemeToggle from "../ThemeToggle/ThemeToggle";
 import { MdAdd } from "react-icons/md";
 import { FiFile } from "react-icons/fi";
-import { IoAnalyticsSharp } from "react-icons/io5";
+// import { IoAnalyticsSharp } from "react-icons/io5";
 import { FiLogIn } from "react-icons/fi";
 import { useLocation, useNavigate } from "react-router-dom";
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 import { useCookies } from "react-cookie";
 import { useAuth } from "../../context/AuthContext";
+
+import { CgProfile } from "react-icons/cg";
+import { RiLogoutCircleRLine } from "react-icons/ri";
+import { CiMenuKebab } from "react-icons/ci";
 
 export default function Navbar() {
   const location = useLocation();
   const navigate = useNavigate();
   const { user, setUser } = useAuth();
   const [cookies, setCookies, removeCookie] = useCookies([]);
+  const [toggleMenu, setToggleMenu] = useState(false);
   return (
     <div className="absolute top-6 md:w-10/12 w-11/12 flex items-center justify-between">
       <h1 className="dark:text-purple-100 hidden md:block text-purple-800 text-xl font-semibold font-Montserrat">
@@ -44,27 +49,56 @@ export default function Navbar() {
             )}
           </>
         )}
-        <motion.button
+        {/* <motion.button
           whileTap={{ scale: 0.85 }}
           onClick={() => navigate("/Analytics")}
           className="dark:text-black text-black p-2 rounded text-sm dark:bg-purple-200 bg-purple-200 hover:ring-2"
-        >
+        > 
           <IoAnalyticsSharp />
-        </motion.button>
+        </motion.button> */}
 
         <ThemeToggle />
         {user && (
-          <motion.button
-            whileTap={{ scale: 0.85 }}
-            className="dark:text-black text-black p-2 rounded text-sm dark:bg-red-200 bg-red-200 hover:ring-2"
-            onClick={() => {
-              setUser(null);
-              removeCookie("jwt");
-              // logout();
-            }}
-          >
-            <FiLogIn />
-          </motion.button>
+          <div className="relative">
+            <motion.button
+              whileTap={{ scale: 0.85 }}
+              className="dark:text-black text-black p-2 rounded text-sm dark:bg-purple-200 bg-purple-200 hover:ring-2"
+              onClick={() => setToggleMenu(!toggleMenu)}
+            >
+              <CiMenuKebab />
+            </motion.button>
+            <AnimatePresence>
+              {toggleMenu && (
+                <motion.div
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  exit={{ opacity: 0 }}
+                  className="absolute w-52 pb-4 pt-4 px-2 dark:bg-zinc-800 bg-gray-100 top-12 right-0 rounded-md dark:text-gray-200"
+                >
+                  <ul>
+                    <li
+                      onClick={() => navigate("/Analytics")}
+                      className="flex items-center gap-2 px-2 py-2 text-sm mb-2 dark:hover:bg-black hover:bg-gray-200 rounded-md transition-colors"
+                    >
+                      <CgProfile />
+                      Profile settings
+                    </li>
+                    <li
+                      onClick={() => {
+                        setUser(null);
+                        removeCookie("jwt");
+                        // logout();
+                      }}
+                      className="flex items-center gap-2 px-2 py-2 text-sm dark:hover:bg-black hover:bg-red-200 rounded-md transition-colors"
+                    >
+                      <RiLogoutCircleRLine />
+                      Logout session
+                    </li>
+                  </ul>
+                </motion.div>
+              )}
+            </AnimatePresence>
+          </div>
         )}
       </div>
     </div>
--- src/Pages/Analytics/Analytics.jsx
+++ src/Pages/Analytics/Analytics.jsx
@@ -1,14 +1,53 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { InformationCard, Navbar, ExpenseTable } from "../../Components";
+import { Navbar } from "../../Components";
 import withAuth from "../../hoc/withAuth";
+import { useAuth } from "../../context/AuthContext";
+import { FiPlus, FiTrash, FiTrash2 } from "react-icons/fi";
+import { BiCategory } from "react-icons/bi";
 
 function Analytics() {
+  const { user } = useAuth();
   return (
     <div className="relative flex h-screen w-full gap-10 justify-center items-center">
       <Navbar />
 
-      <p className="text-3xl dark:text-white">Testing Page</p>
+      {/* <div className="dark:bg-zinc-800 dark:text-gray-100 bg-gray-200 h-96 w-96 rounded-xl"> */}
+      <div className="dark:text-gray-100 w-96 rounded-xl">
+        <h1 className="text-lg font-Montserrat">Manage caterogies</h1>
+        <div className="relative w-full flex justify-between mt-4 gap-4 items-center">
+          <BiCategory className="absolute top-4 left-2 dark:text-gray-300 text-gray-700" />
+          <input
+            type="text"
+            className="mt-1 w-full rounded-md border p-2 pl-8 outline-blue-400 hover:ring-2 hover:ring-blue-300 focus:ring-0 dark:text-white dark:border-zinc-800 dark:bg-zinc-900 text-black bg-white text-sm transition-colors ease-in-out duration-300"
+            placeholder="add new category"
+          />
+          <button className="p-2.5 dark:bg-zinc-800 bg-black rounded-full text-white">
+            <FiPlus />
+          </button>
+        </div>
+
+        <div className="w-full mt-4">
+          <div className="w-full flex justify-between border dark:border-zinc-800 dark:bg-zinc-900 px-2 py-2 rounded-md transition-all">
+            <h1>Commercial Transportation</h1>
+            <button className="text-red-400">
+              <FiTrash2 />
+            </button>
+          </div>
+          <div className="w-full flex justify-between border dark:border-zinc-800 dark:bg-zinc-900 px-2 py-2 rounded-md transition-all mt-2">
+            <h1>Personal Transportation</h1>
+            <button className="text-red-400">
+              <FiTrash2 />
+            </button>
+          </div>
+          <div className="w-full flex justify-between border dark:border-zinc-800 dark:bg-zinc-900 px-2 py-2 rounded-md transition-all mt-2">
+            <h1>Transportation</h1>
+            <button className="text-red-400">
+              <FiTrash2 />
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
